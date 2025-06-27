b61818abbdecdf09dc3bcb3473287fc8b182d672
seungjulee97 <seungju9788@gmail.com>
2025-06-27 13:24:04 +0900

    Update day16.py

--- day16.py
+++ day16.py
@@ -11,7 +11,7 @@ backgroundColor="#2E86C1"
 secondaryBackgroundColor="#AED6F1"
 textColor="#FFFFFF"
 font="monospace"
-"")
+""")
 
 number = st.sidebar.slider('숫자를 선택하세요:', 0, 10, 5)
 st.write('슬라이더 위젯에서 선택된 숫자:', number)
