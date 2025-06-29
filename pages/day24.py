e91530ff39579fe5486fdb6d53b966d750cc4cb8
leesj <seungju9788@gmail.com>
2025-06-29 15:22:45 +0900

    add day24

---
+++ pages/day24.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from time import time
+
+st.title('st.cache')
+
+
+a0 = time()
+st.subheader('st.cache 사용')
+
+@st.cache_data
+def load_data_a():
+    df = pd.DataFrame(
+        np.random.rand(2000000, 5),
+        columns=['a', 'b', 'c', 'd', 'e']
+    )
+    return df
+
+st.write(load_data_a()) 
+a1 = time()
+st.info(a1-a0)
+
+b0 = time()
+st.subheader('st.cache 미사용')
+
+def load_data_b():
+    df= pd.DataFrame(
+        np.random.rand(2000000, 5),
+        columns=['a', 'b', 'c', 'd', 'e']
+    )
+    return df
+
+st.write(load_data_b())
+b1 = time()
+st.info(b1-b0)
