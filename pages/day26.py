129152f9c200a25c62fa17ee83a07111117b4639
leesj <seungju9788@gmail.com>
2025-06-29 19:27:07 +0900

    add day27

---
+++ pages/day26.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+
+st.title('🏀 Bored API 앱')
+
+st.sidebar.header('입력')
+selected_type = st.sidebar.selectbox('활동 유형 선택', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
+
+c1, c2 = st.columns(2)
+with c1:
+    with st.expander('이 앱에 대하여'):
+        st.write('지루하신가요? **Bored API앱**은 지루할 때 할 수 있는 활동을 제안합니다. 이 앱은 Bored API에 의해 구동됩니다.')
+
+with c2:
+    with st.expander('JSON 데이터'):
+        st.write(suggested_activity)
+
+st.header('제안된 활동')
+st.info(suggested_activity['activity'])
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric(label='참가자 수', value=suggested_activity['participants'], delta= '')
+with col2:
+    st.metric(label='활동 유형', value=suggested_activity['type'].capitalize(), delta='')
+with col3:
+    st.metric(label='가격', value=suggested_activity['price'], delta='')
---
+++ pages/day27.py
@@ -0,0 +1,89 @@
+import json 
+import streamlit as st
+from pathlib import Path
+
+from streamlit_elements import elements, dashboard, mui, editor, media, lazy, sync, nivo
+
+st.set_page_config(layout="wide")
+
+with st.sidebar:
+    st.title("🗓️ #30DaysOfStreamlit")
+    st.header("Day 27 - Streamlit Elements")
+    st.write("Streamlit Elements를 사용하여 드래그 가능하고 크기 조절 가능한 대시보드 만들기")
+    st.write("---")
+
+    media_url = st.text_input("미디어 URL", value="https://www.youtube.com/watch?v=vIQQR_yq-8I")
+
+if "data" not in st.session_state:
+    st.session_state.data = Path("data.json").read_text()
+
+
+layout = [
+    dashboard.Item("editor", 0, 0, 6, 3),
+    dashboard.Item("chart", 6, 0, 6, 3),
+    dashboard.Item("media", 0, 2, 12, 4)
+]
+
+with elements("demo"):
+
+    with dashboard.Grid(layout, draggableHandle=".draggable"):
+        with mui.Card(key="editor", sx={"display": "flex", "flexDirection": "column" }):
+            mui.CardHeader(title="Editor", className="draggable")
+
+        with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+            editor.Monaco(
+                defaultValue=st.session_state.data, 
+                language="json",
+                onChange=lazy(sync("data"))
+            )
+
+        with mui.CardActions:
+            mui.Button("변경 사항 적용", onClick=sync())
+        with mui.Card(key="chart", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="차트", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                nivo.Bump(
+                    data=json.loads(st.session_state.data),
+                    colors={ "scheme": "spectral" },
+                    lineWidth=3,
+                    activeLineWidth=6,
+                    inactiveLineWidth=3,
+                    inactiveOpacity=0.15,
+                    pointSize=10,
+                    activePointSize=16,
+                    inactivePointSize=0,
+                    pointColor={ "theme": "background" },
+                    pointBorderWidth=3,
+                    activePointBorderWidth=3,
+                    pointBorderColor={ "from": "serie.color" },
+                    axisTop={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": -36
+                    },
+                    axisBottom={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": 32
+                    },
+                    axisLeft={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "ranking",
+                        "legendPosition": "middle",
+                        "legendOffset": -40
+                    },
+                    margin={ "top": 40, "right": 100, "bottom": 40, "left": 60 },
+                    axisRight=None,
+                )
+            with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
+             mui.CardHeader(title="미디어 플레이어", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                media.Player(url=media_url, width="100%", height="100%", controls=True)
